e4314c7c83485d10e21f76c1a29061cef25d6677
Mark Flint <mail@markflint.net>
2021-12-05 13:30:54 +0000

    initial commit, no remote repo yet!

---
+++ src/index.php
@@ -0,0 +1,57 @@
+<?php
+//phpinfo();
+//exit;
+//xdebug_info();
+//exit;
+
+/************************/
+/*  If the debugger won't work try rebuild php, docker compose up -d --no-deps --build php
+/************************/
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            body {
+                font-family: "Arial", sans-serif;
+                font-size: larger;
+            }
+
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+        <?php
+		//xdebug_break();
+		$x=1;
+		$y=2;
+		//exit;
+		
+        $connection = new PDO('mysql:host=mysql;dbname=demo;charset=utf8', 'root', 'root');
+        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'demo'");
+        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        if (empty($tables)) {
+            echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
+        } else {
+            echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
+            echo '<ul class="center">';
+            foreach ($tables as $table) {
+                echo "<li>{$table}</li>";
+            }
+            echo '</ul>';
+        }
+        ?>
+    </body>
+</html>
+
